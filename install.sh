fb750c3869a05fdb193d7dc73f7ec926cb74432c
Sushant Mahajan <sushant@zen3>
2016-08-16 21:36:01 +0530

    created draft installer

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 ! [[ -e "$1" ]] && { echo "$1 does not exist"; exit 2; }
 repo=$(pwd)
 prefix=$1
-for i in $(ls)
+for i in $(ls -a)
 do
-	[[ "$i" = "install.sh" ]] && continue
-	{ echo ${prefix}/$i; ln -sf ${repo}/$i ${prefix}/$i; }
+	[[ "$i" = "install.sh" || "$i" = ".gitignore" || ! -f "$i" || -e "${prefix}/$i" ]] && continue
+	echo "${prefix}/$i -> ${repo}/$i";
+	ln -sf ${repo}/$i ${prefix}/$i;
 done
-
+ln -sf ${repo}/.vim ${prefix}/.vim
+ln -sf ${repo}/.config/terminator/config ${prefix}/.config/terminator/config
+ln -sf ${repo}/.oh-my-zsh/themes/sushant.zsh-theme ${prefix}/.oh-my-zsh/themes/sushant.zsh-theme
